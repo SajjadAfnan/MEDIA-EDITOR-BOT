ad27f600fba3cd81022f1931ec43e84b1fb422c8
SajjadAfnan <71611618+SajjadAfnan@users.noreply.github.com>
2021-11-25 23:09:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from pyromod import listen
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
+    from configu import Config
 else:
     from configu import Config
 
